aafc987b1aa320113e14e93174799b65561ab053
ChinoISC <oscar010394@gmail.com>
2026-02-09 09:51:55 -0600

    Fix

--- src/main/java/com/example/proyecto/demo/config/CorsConfig.java
+++ src/main/java/com/example/proyecto/demo/config/CorsConfig.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsConfig {
 
     @Bean
